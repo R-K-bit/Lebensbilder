44f1c403a2a3c5973af24bd2ae9c060bea05eb8f
kndig <rebecca.kuendig@stud.unibas.ch>
2021-04-01 15:48:29 +0200

    changed button conditions

--- javascript.js
+++ javascript.js
@@ -8,7 +8,9 @@ var ideas = [
 	'Mann spinnt', 'Mann webt', 'Mann flechtet', 'Mann holt Wasser', 'Mann trägt Tongefäss', 'Mann töpfert', 'Mann mahlt Mehl', 'Mann säht aus', 'Frau als Kriegerin', 'Frau beim Roden',
 	'Frau bearbeitet Holz', 'Frau in Führungsposition', 'Frau reitet', 'Frau fährt Wagen', 'Frau betreibt Handel', 'Mann näht Textilien', 'Mann umarmt Kind', 'Frau umarmt Kind',
 	'Frau im Bergbau', 'Kind beim Wurzelgraben', 'Frau kehrt von Jagd zurück', 'Frau belädt Ochsenkarren', 'Kind webt', 'Kind trägt Jagdbeute', 'Kinder spielen zusammen', 'Kinder holen Wasser',
-	'Kinder passen auf jüngere Kinder auf', 'Kinder sammeln Feuerholz', 'Kinder erlernen die Produktion von Steinwerkzeugen', 'Kind näht Fell', 'Kind kümmert sich um alte Person'
+	'Kinder passen auf jüngere Kinder auf', 'Kinder sammeln Feuerholz', 'Kinder erlernen die Produktion von Steinwerkzeugen', 'Kind näht Fell', 'Kind kümmert sich um alte Person',
+	'Keltenfürstin trinkt mit Gästen', 'Keltenfürstin fährt auf ihrem Wagen', 'Keltenfürst erfüllt Priesterrolle', 'Frauen beim sportlichen Wettkampf', 'Mann betrachtet Waren',
+	'Verheiratung Mann als Beziehungspflege zwischen den Frauen'
 
 ]
 
